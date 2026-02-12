af2a9b09a95f83dd3af41cabdc01a0cf8bc74cca
Praveen Baskandi <praveenbaskandi@Praveens-MacBook-Air.local>
2026-02-12 12:47:34 +0530

    minor fixes and changes

--- src/screens/useCryptoList.ts
+++ src/screens/useCryptoList.ts
@@ -1,30 +1,34 @@
-import { useEffect, useMemo } from 'react';
-import { appStateManager } from '../infrastructure/services/AppStateManager';
-import { webSocketClient } from '../infrastructure/services/WebSocketClient';
-import { useAppSelector } from '../redux/hooks';
-import { Ticker } from '../redux/types';
+import { useEffect, useMemo } from "react";
+import { webSocketClient } from "../network/WebSocketClient";
+import { useAppSelector } from "../redux/hooks";
+import { Ticker } from "../redux/types";
+import { appStateManager } from "../services/AppStateManager";
 
 export const useCryptoList = () => {
-    const tickers = useAppSelector((state) => state.crypto.tickers);
-    const connectionStatus = useAppSelector((state) => state.crypto.connectionStatus);
+  const tickers = useAppSelector((state) => state.crypto.tickers);
+  const connectionStatus = useAppSelector(
+    (state) => state.crypto.connectionStatus,
+  );
 
-    useEffect(() => {
-        // Initialize services
-        webSocketClient.connect();
-        const appStateCleanup = appStateManager.init();
+  useEffect(() => {
+    // Initialize services
+    webSocketClient.connect();
+    const appStateCleanup = appStateManager.init();
 
-        return () => {
-            webSocketClient.disconnect();
-            appStateCleanup();
-        };
-    }, []);
-
-    const tickerList = useMemo(() => {
-        return (Object.values(tickers) as Ticker[]).sort((a, b) => a.symbol.localeCompare(b.symbol));
-    }, [tickers]);
-
-    return {
-        tickerList,
-        connectionStatus,
+    return () => {
+      webSocketClient.disconnect();
+      appStateCleanup();
     };
+  }, []);
+
+  const tickerList = useMemo(() => {
+    return (Object.values(tickers) as Ticker[]).sort((a, b) =>
+      a.symbol.localeCompare(b.symbol),
+    );
+  }, [tickers]);
+
+  return {
+    tickerList,
+    connectionStatus,
+  };
 };
